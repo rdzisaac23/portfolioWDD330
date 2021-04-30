c3cf8efd67eb73bf41f3e93b0e65622f8d44c8ee
Isaac Rodriguez <64040285+rdzisaac23@users.noreply.github.com>
2021-04-30 13:03:17 -0400

    Week One

---
+++ js/nav.js
@@ -0,0 +1,41 @@
+const links = [{
+        label: "Week notes",
+        url: "w01/index.html"
+    },
+    {
+        label: "Week2",
+        url: "#"
+    },
+    {
+        label: "Challenge 1: Todo",
+        url: "#"
+    }
+]
+
+function unorderList() {
+
+    for (var i = 0; i < links.length; i++) {
+        //create li items
+        let list = document.createElement('li');
+
+        //create a
+        let a = document.createElement('a');
+
+        // Set contents
+        a.textContent = links[i].label;
+        a.setAttribute('href', links[i].url);
+
+        // add them to the ol
+        list.appendChild(a);
+        console.log(list);
+
+        document.querySelector('ul#unorder-list').appendChild(list);
+    }
+
+};
+
+
+links.forEach(unorderList => {});
+
+console.log(links);
+unorderList();
